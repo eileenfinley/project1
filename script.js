3abace60a897af22ef8bc85cdba6a3c0b13ece21
eileenfinley <66222893+eileenfinley@users.noreply.github.com>
2023-11-07 23:57:08 +0000

    responsive navbar + adjusted pictures

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const formElement = document.querySelector("#form");
-
-formElement.addEventListener("submit",function (e){
+const myJSONText = document.getElementById("finalJSONText");
+myJSONText.innerHTML = localStorage.getItem("JSONInfo");
+// get the form object
+const myForm = document.querySelector("#form");
+// add event listener for the submit event
+myForm.addEventListener("submit", function (e) {
+    // get the name and email object's value
     let name = document.querySelector("#name").value;
     let email = document.querySelector("#email").value;
-
-    const formData = {
-        name: name,
-        email: email
+    // if the name is empty
+    if(name==null || name==""){
+        alert("Enter a name");
+        // do not let the submit happen
+        e.preventDefault();
+        return;
     }
-
-    const jsonDisplay = document.getElementById("jsonDisplay");
-    jsonDisplay.innerHTML = "JSON displayed:<br>" + JSON.stringify(formData);
+    // if the email is empty
+    if(email == null || email == ""){
+        alert("Enter an email");
+        // do not let the submit happen
+        e.preventDefault();
+        return;
+    }
+    // creating object and JSON from the form info
+    const personInfo = {Name:name, Email:email};
+    let myTextJSON = JSON.stringify(personInfo);
+    // storing them in the local computer
+    localStorage.setItem("JSONInfo", myTextJSON);
 });

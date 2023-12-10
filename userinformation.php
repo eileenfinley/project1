0ed105510ac5719f08cd6a6b8e838894f1c4ddad
Eileen Finley <ekf631220@gmail.com>
2023-12-10 17:08:13 -0600

    alert added to shipping

--- userinformation.php
+++ userinformation.php
@@ -18,16 +18,22 @@
             $creditCard = $_POST['cc'];
             $username = $_SESSION["username"];
 
-            $sql = "INSERT INTO orderinformation (username, user_address, user_city, user_state, credit_card) VALUES (?, ?, ?, ?, ?)";
-            
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sssss", $username, $address, $city, $state, $creditCard);
+            if($address && $city && $state && $creditCard){
+                $sql = "INSERT INTO orderinformation (username, user_address, user_city, user_state, credit_card) VALUES (?, ?, ?, ?, ?)";
+                
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("sssss", $username, $address, $city, $state, $creditCard);
 
-            if ($stmt->execute()) {
-                header("Location: index.php");
-            } else {
-                echo "Error inserting user address: " . $stmt->error;
+                if ($stmt->execute()) {
+                    header("Location: index.php");
+                } else {
+                    echo "Error inserting user address: " . $stmt->error;
+                }               
+            }else{
+                echo("<div class = 'alert alert-invalid-info'>Please fill in all fields</div>");
             }
+
+
         }
     ?>
 

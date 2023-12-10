89fb92d8f6e440a9e45332c034b0c9905c46f0c2
Eileen Finley <ekf631220@gmail.com>
2023-12-10 16:56:05 -0600

    user information updated

--- userinformation.php
+++ userinformation.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel = "stylesheet" href = "loginstyle.css" media = "screen and (min-width:769px)"/>-->
     <title>information</title>
 </head>
 <body>
     <?php
         include("database.php");
+        $username = $_SESSION["username"];
 
         if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])){
             $address = $_POST['address'];
@@ -15,10 +17,10 @@
             $state = $_POST['state'];
             $creditCard = $_POST['cc'];
 
-            $sql = "INSERT INTO orderinformation (user_address, user_city, user_state, credit_card) VALUES (?, ?, ?, ?)";
+            $sql = "INSERT INTO orderinformation (username, user_address, user_city, user_state, credit_card) VALUES (?, ?, ?, ?, ?)";
             
             $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ssss", $address, $city, $state, $creditCard);
+            $stmt->bind_param("sssss", $username, $address, $city, $state, $creditCard);
 
             if ($stmt->execute()) {
                 echo "User address inserted successfully!";
@@ -28,12 +30,20 @@
         }
     ?>
 
-    <form action = "userinformation.php" method = "post">
-        Address <input type = "text" name = "address"/>
-        City <input type = "text" name = "city"/>
-        State <input type = "text" name = "state"/>
-        Credit Card Number <input type = "text" name = "cc" />
-        <input type = "submit" name = "submit">
-    </form>
+    <div class = "user"> 
+        <div class = "information">
+            <form action = "userinformation.php" method = "post">
+                <label>Shipping Information</label>
+                <input type = "text" name = "address" placeholder= "Address"/>
+                <input type = "text" name = "city" placeholder = "City"/>
+                <input type = "text" name = "state" placeholder="State"/>
+                <input type = "text" name = "cc" placeholder="Credit Card Number"/>
+                <input type = "submit" name = "submit">
+                <p>Already submitted information? <a href = "index.php">Click here to back out</a></p> 
+            </form>   
+        </div>
+ 
+    </div>
+
 </body>
 </html>
